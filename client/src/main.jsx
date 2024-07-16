d04898f1f2cf327aecd1463cf892a1809426c480
Braghadeesh R A <122204941+Braghadeesh05@users.noreply.github.com>
2024-07-16 22:01:35 +0530

    client updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter} from "react-router-dom"
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'jquery/dist/jquery.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
